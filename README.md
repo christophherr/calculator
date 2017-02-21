85c4f9cca0b017b341f4103680a7e288fc6f9d79
christophherr <info@christophherr.com>
2017-02-21 15:42:54 -0500

    Complete rewrite.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# calculator
+# JavaScript Calculator
+## Using Vue.js, math.js and Bulma.io
 JavaScrip Calculator for Free Code Camp's Front End Certification
 
 Functional CodePen: http://codepen.io/c-herr/full/rLNvpy
--- assets/app.js
+++ assets/app.js
@@ -1,50 +1,85 @@
-$(document).ready(function(){
-	var testNumLength = function(number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Err");
+var app = new Vue({
+    el: '#calculator',
+    data: {
+        calculated: false,
+        result: ''
+    },
+    mounted: function() {
+        window.addEventListener('keyup', function(event) {
+            if (event.keyCode == 109 || event.keyCode == 189) {
+                app.show('-');
             }
+            if (event.keyCode == 107) {
+                app.show('+');
+            }
+            if (event.keyCode == 111 || event.keyCode == 191 || event.keyCode == 58) {
+                app.show('/');
+            }
+            if (event.keyCode == 106 || event.keyCode == 88) {
+                app.show('*');
+            }
+            if (event.keyCode == 187 || event.keyCode == 61) {
+                app.show('=');
+            }
+            if (event.keyCode == 48) {
+                app.showNumbers(0);
+            }
+            if (event.keyCode == 49) {
+                app.showNumbers(1);
+            }
+            if (event.keyCode == 50) {
+                app.showNumbers(2);
+            }
+            if (event.keyCode == 51) {
+                app.showNumbers(3);
+            }
+            if (event.keyCode == 52) {
+                app.showNumbers(4);
+            }
+            if (event.keyCode == 53) {
+                app.showNumbers(5);
+            }
+            if (event.keyCode == 54) {
+                app.showNumbers(6);
+            }
+            if (event.keyCode == 55) {
+                app.showNumbers(7);
+            }
+            if (event.keyCode == 56) {
+                app.showNumbers(8);
+            }
+            if (event.keyCode == 57) {
+                app.showNumbers(9);
+            }
+            if (event.keyCode == 110 || event.keyCode == 108 || event.keyCode == 190 || event.keyCode == 194) {
+                app.showNumbers('.');
+            }
+            if (event.keyCode == 8 || event.keyCode == 27) {
+                app.clear();
+            }
+            if (event.keyCode == 61 || event.keyCode == 13 || event.keyCode == 187) {
+                app.calculate();
+            }
+        }, true);
+    },
+    methods: {
+        showNumbers: function(value) {
+            if (this.calculated) {
+                this.clear();
+                this.calculated = false;
+            }
+            this.show(value);
+        },
+        show: function(value) {
+            this.calculated = false;
+            this.result += value;
+        },
+        calculate: function() {
+            this.calculated = true;
+            this.result = math.eval(this.result);
+        },
+        clear: function() {
+            this.result = '';
         }
-    };
-	var number = "";
-    var newnumber = "";
-    var operator = "";
-    var totaldiv = $("#total");
-    totaldiv.text("0");
-    $("#numbers a").not("#clear,#clearall").click(function(){
-		number += $(this).text();
-		totaldiv.text(number);
-		testNumLength(number);
-    });
-    $("#operators a").not("#equals").click(function(){
-		operator = $(this).text();
-		newnumber = number;
-		number = "";
-		totaldiv.text("0");
-    });
-    $("#clear,#clearall").click(function(){
-		number = "";
-		totaldiv.text("0");
-		if ($(this).attr("id") === "clearall") {
-			newnumber = "";
-		}
-    });
-    //Add your last .click() here!
-    $("#equals").click(function(){
-    	if (operator === "+"){
-    		number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(10);
-    	} else if (operator === "-"){
-    		number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(10);
-    	} else if (operator === "÷"){
-    		number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(10);
-    	} else if (operator === "×"){
-    		number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(10);
-    	}
-    	totaldiv.text(number);
-    	testNumLength(number);
-    	number = "";
-    	newnumber = "";
-    });
+    }
 });
